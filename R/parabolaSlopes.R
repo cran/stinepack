a210a156b6db802673b1638e33530422f585d291
Tomas Johannesson <tj@vedur.is>
2012-10-29 00:00:00 +0000

    version 1.3

--- R/parabolaSlopes.R
+++ R/parabolaSlopes.R
@@ -9,7 +9,9 @@ function (x, y)
     if (m == 2) {
         yp <- rep(dydx, 2)
     } else {
-        yp <- c(2*dydx[1]-yp[2],(dydx[-m1]*dx[2:m1] + dydx[2:m1]*dx[-m1])/(dx[2:m1] + dx[-m1]),2*dydx[m1]-yp[m1])
+        yp <- c((dydx[1]*(2*dx[1]+dx[2])-dydx[2]*dx[1])/(dx[1]+dx[2]),
+        (dydx[-m1]*dx[-1] + dydx[-1]*dx[-m1])/(dx[-1] + dx[-m1]),
+        (dydx[m1]*(2*dx[m1]+dx[m1-1])-dydx[m1-1]*dx[m1])/(dx[m1]+dx[m1-1]))
     }
     yp
 }
